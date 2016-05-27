268052f1f533372ba0efdb7b30dd7d68b396df94
dacang <1061300519@qq.com>
2016-05-27 13:13:00 +0800

    Add files via upload

---
+++ JDBC02/src/com/vae/jdbc/JdbcQuey.java
@@ -0,0 +1,91 @@
+package com.vae.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JdbcQuey {
+
+	//数据库连接地址，连接字符串
+	private final static String URL = "jdbc:mysql://localhost:3306/mysql";
+	//用户名
+	public final static String USERNAME = "root";
+	//密码
+	public final static String PASSWORD = "10ASDasd";
+	//加载的驱动程序类（这个类就在我们导入的jar包中）
+	public final static String DRIVER = "com.mysql.jdbc.Driver";
+
+	/**
+	 * @方法描述 TODO    执行插入记录sql
+	 * @param person 
+	 *  
+	 */
+	public static void insert(Person person) {
+		Connection conn = null;
+		PreparedStatement pst = null;
+
+		//问号可以理解为占位符，有几个问号就代表要插入几个列，这样看来sql代码就比较简洁。
+		String sql = "insert  into person(id,name,age,description) value(?,?,?,?)";
+
+		try {
+			Class.forName(DRIVER);
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			pst = conn.prepareStatement(sql);// 会抛出SQLException
+			//问号设值，参数1代表第一个问号的位置，以此类推。
+			pst.setObject(1, person.getpId());
+			pst.setObject(2, person.getpName());
+			pst.setObject(3, person.getpAge());
+			pst.setObject(4, person.getpDescription());
+			pst.executeUpdate();
+			pst.close();
+			conn.close();
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person p = new Person(13, "dddd", 23, "hhhhh");
+		//将整个person对象进去，代表的是数据库中的一条记录。
+		insert(p);
+		query();
+
+	}
+
+	//方法：查询操作
+	public static void query() {
+		try {
+			Class.forName(DRIVER);
+			Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			String sql = "select id,name,age,description from person";
+			Statement state = conn.createStatement();
+			//执行查询并返回结果集
+			ResultSet rs = state.executeQuery(sql);
+			while (rs.next()) { //通过next来索引：判断是否有下一个记录
+
+				int id = rs.getInt(1); //方法：int java.sql.ResultSet.getInt(int columnIndex) throws SQLException
+
+				String name = rs.getString(2);
+				int age = rs.getInt(3);
+				String description = rs.getString(4);
+				System.out.println("id=" + id + ",name=" + name + ",age=" + age + ",description=" + description);
+			}
+			rs.close();
+			state.close();
+			conn.close();
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ JDBC03/src/com/vae/jdbc/JdbcQuey.java
@@ -0,0 +1,92 @@
+package com.vae.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class JdbcQuey {
+
+	//数据库连接地址
+	private final static String URL = "jdbc:mysql://localhost:3306/mysql";
+	//用户名
+	public final static String USERNAME = "root";
+	//密码
+	public final static String PASSWORD = "10ASDasd";
+	//加载的驱动程序类（这个类就在我们导入的jar包中）
+	public final static String DRIVER = "com.mysql.jdbc.Driver";
+	private static Connection conn;
+	private static PreparedStatement pst;
+
+	/**
+	 * @方法描述 TODO 方法：  查询操作
+	 *  
+	 */
+	/**
+	 * @方法描述 TODO  插入和查询，事务管理
+	 *  
+	 */
+	public static void insertAndQuery() {
+		conn = null;
+		pst = null;
+		try {
+			Class.forName(DRIVER);
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			//1.设置为手动提交事务
+			conn.setAutoCommit(false);
+			String sql1 = "insert into user(name,pwd)values(?,?)";
+			String sql2 = "update user set pwd=? where name=?";
+
+			pst = conn.prepareStatement(sql1);
+			pst.setString(1, "smyhvae");
+			pst.setString(2, "007");
+			pst.executeUpdate();
+
+			pst = conn.prepareStatement(sql2);
+			pst.setString(1, "008");
+			pst.setString(2, "smyh");
+			pst.executeUpdate();
+
+			//2.如果所有sql语句成功，则提交事务
+			conn.commit();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("调用Class的forName方法时，找不到指定的类");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			/**
+			 * 关闭资源
+			 * 
+			 * @param 
+			 */
+			try {
+
+				if (pst != null) {
+					// 关闭Statement
+					pst.close();
+
+					pst = null;
+				}
+				if (conn != null) {
+					// 关闭Connection
+					conn.close();
+
+					conn = null;
+				}
+				//3.只要有一个sql语句出现错误，则将事务回滚 
+				conn.rollback();
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		insertAndQuery();
+
+	}
+}
